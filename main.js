5239aae0814bbeb94f1581caff7f40496e8b0e51
pluis1570 <laur.luyp@hotmail.com>
2018-08-21 14:48:56 +0200

    adjusting files for JS

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 function init() {
     Tabletop.init({
-      key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+      key: 'https://docs.google.com/spreadsheets/d/19c5VxyfV5P3-4jUwramk5fiyFLm4IPGqqLqo8_6f7-o/pubhtml',
       callback: function (tabletopData, tabletop) {
         console.log(tabletopData)
    
         var app4 = new Vue({
           el: '#app-4',
+          data: {
+            list: tabletopData,
+          }
+        })
+        var box2 = new Vue({
+          el: '#box2',
+          data: {
+            list: tabletopData,
+          }
+        })
+        var box3 = new Vue({
+          el: '#box3',
           data: {
             list: tabletopData,
           }
