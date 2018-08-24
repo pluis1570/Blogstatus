20790a58505f2576174da0d872e8fda9c7c0a894
pluis1570 <laur.luyp@hotmail.com>
2018-08-24 12:09:53 +0200

    final adjustment project 1

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function init() {
+
+
   Tabletop.init({
     key: 'https://docs.google.com/spreadsheets/d/19c5VxyfV5P3-4jUwramk5fiyFLm4IPGqqLqo8_6f7-o/pubhtml',
     callback: function (tabletopData, tabletop) {
       console.log(tabletopData)
- 
+
+
+
+
       var app4 = new Vue({
         el: '#app-4',
         data: {
